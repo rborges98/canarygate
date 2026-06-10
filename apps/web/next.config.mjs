db78cb5db56865df335a7e35194bc800d97ffb72
rborges98 <rcborges98@gmail.com>
2026-06-09 21:55:29 -0300

    chore(next.config): add env list

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,20 @@ import nextra from 'nextra'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    API_URL: process.env.API_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    MICROSOFT_CLIENT_ID: process.env.MICROSOFT_CLIENT_ID,
+    MICROSOFT_CLIENT_SECRET: process.env.MICROSOFT_CLIENT_SECRET,
+    CORS_ALLOWED_ORIGINS: process.env.CORS_ALLOWED_ORIGINS
+  },
   headers: async () => [
     {
       source: '/(.*)',
